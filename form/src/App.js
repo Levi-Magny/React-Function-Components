c6da8b8430673bdada2918d6002d581730b2142f
Levi-Magny <lemmagny@gmail.com>
2021-08-30 14:14:40 -0300

    Introdução

---
+++ form/src/App.js
@@ -0,0 +1,14 @@
+import { Fragment } from 'react';
+import FormularioCadastro from './components/FormularioCadastro/FormularioCadastro';
+import './App.css';
+
+function App() {
+  return (
+    <Fragment>
+      <h1>Formulario de Cadastro</h1>
+      <FormularioCadastro/>
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ form/src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function FormularioCadastro() {
+    return (
+        <form>
+            <input type="text" name="" id="" />
+        </form>
+    )
+}
+
+export default FormularioCadastro;
